6abf65927ef905128cde86c128fccfc3ce99a2d8
sfrugelowe <sfrugelowe@gmail.com>
2024-11-05 11:53:51 -0600

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -82,6 +82,7 @@ def authorized():
     if request.args.get('state') != session.get("state"):
         return redirect(url_for("home"))  # No-OP. Goes back to Index page
     if "error" in request.args:  # Authentication/Authorization failure
+        app.logger.warning("Invalid login attempt. No code returned")
         return render_template("auth_error.html", result=request.args)
     if request.args.get('code'):
         cache = _load_cache()
@@ -92,6 +93,7 @@ def authorized():
             scopes=Config.SCOPE,
             redirect_uri=url_for('authorized', _external=True, _scheme='https'))
         if "error" in result:
+            app.logger.warning("Invalid login attempt. Code returned")
             return render_template("auth_error.html", result=result)
         session["user"] = result.get("id_token_claims")
         # Note: In a real app, we'd use the 'name' property from session["user"] below
@@ -99,6 +101,7 @@ def authorized():
         user = User.query.filter_by(username="admin").first()
         login_user(user)
         _save_cache(cache)
+        app.logger.warning("admin logged successfully.")
     return redirect(url_for('home'))
 
 @app.route('/logout')
